e8ac532eb65eefe4deb8b7290258f9340ac692be
girishhh <girikulkarni03@gmail.com>
2016-03-18 22:14:12 +0530

    mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
 	def send_mail(mail_id)
 		p "HELLLLLLLLLO"
 		p mail_id
-		mail(to: mail_id, subject: "Thanks for using blog")
+		mail(to: [mail_id], subject: "Thanks for using blog")
   end
 end
